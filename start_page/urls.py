b26b80b6231262437c4387617ef568c2876804fe
niaza64 <niazahmad9620804@gmail.com>
2022-11-27 19:53:48 +0500

    First Changes

--- start_page/urls.py
+++ start_page/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("summer", views.summer, name="summer"),
+    path("winter", views.winter, name="winter")
 ]
--- start_page/views.py
+++ start_page/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "start_page/index.html")
+
+def summer(request):
+    return render(request, "start_page/summer.html")
+
+def winter(request):
+    return render(request, "start_page/winter.html")
