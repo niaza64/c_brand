6a0a287f670147a51e9e29b6d4825172a2c75e11
niaza64 <niazahmad9620804@gmail.com>
2022-11-29 01:19:10 +0500

    mongo added

--- start_page/models.py
+++ start_page/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class cloth(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    count = models.IntegerField(blank=True, null=True)
--- start_page/views.py
+++ start_page/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import cloth
 # Create your views here.
 def index(request):
     return render(request, "start_page/index.html")
 
 def summer(request):
-    return render(request, "start_page/summer.html")
+    return render(request, "start_page/summer.html", {
+        "cloths": cloth.objects.all()
+    }
+    )
 
 def winter(request):
     return render(request, "start_page/winter.html")
